e6867e143f053db638e83b17861288472f8675f4
Thomas J. Fan <thomasjpfan@gmail.com>
2024-03-20 14:46:43 -0700

    Use text based keyring

--- python_keyring/keyring/custom.py
+++
@@ -1,6 +0,0 @@
-from os import getenv
-from keyrings.cryptfile.cryptfile import CryptFileKeyring
-
-class CustomCryptFileKeyring(CryptFileKeyring):
-    def __init__(self):
-        self._keyring_key = getenv("KEYRING_CRYPTFILE_PASSWORD")
